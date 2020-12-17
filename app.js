5f9269acec7ae9fa505a4661dbd1b823efbd91c2
HungTrinh99 <minhhung.it99@gmail.com>
2020-12-18 02:04:21 +0700

    Add: connect application to MySq

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "elearning",
+});
 
 const app = express();
 app.use(express.static(__dirname + "/public"));
@@ -8,6 +16,14 @@ app.engine("hbs", exphbs({ extname: ".hbs", defaultLayout: "main.hbs" }));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
+  db.connect();
+
+  db.query("select * from users", function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+  });
+
+  db.end();
   res.render("home");
 });
 
