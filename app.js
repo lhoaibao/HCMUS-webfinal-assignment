d3382f34ba579a605d8cd53fe4226b03493d8434
HungTrinh99 <minhhung.it99@gmail.com>
2020-12-21 02:44:26 +0700

    Add: Error handling page with 404 and 500 error

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.set("views", path.join(__dirname, "views"));
 // Middleware
 app.use("/", routing);
 
+// Default root
+app.use((req, res) => {
+  res.render("404", {
+    layout: false,
+  });
+});
+
+// Handle Error
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).render("500", {
+    layout: false,
+  });
+});
+
+// App is listening at PORT 3000
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Application is running on PORT ${PORT}`);
