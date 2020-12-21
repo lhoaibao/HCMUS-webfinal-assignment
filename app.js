90ccc22726c8ff47c6cd4c2ac52c023cc3f41034
Le Hoai Bao <lehoaibao081999@gmail.com>
2020-12-21 19:45:34 +0700

    add error.controller, route.controller, view.controller and remove not necessary file

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
 // Import module
 const express = require("express");
-const exphbs = require("express-handlebars");
-const routing = require("./routing");
-const path = require("path");
 const app = express();
 require("express-async-errors");
 
 // Static file setup
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static( "./public"));
 
-// view engine setup
-app.engine("hbs", exphbs({ extname: ".hbs", defaultLayout: "layout.hbs" }));
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
-
-// Middleware
-app.use("/", routing);
-
-// Default root
-app.use((req, res) => {
-  res.render("404", {
-    layout: false,
-  });
-});
-
-// Handle Error
-app.use((err, req, res, next) => {
-  console.log(err.stack);
-  res.status(500).render("500", {
-    layout: false,
-  });
-});
+require('./controllers/view.controller')(app)
+require('./controllers/route.controller')(app)
+require('./controllers/error.controller')(app)
 
 // App is listening at PORT 3000
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Application is running on PORT ${PORT}`);
+  console.log(`Application is running at http://localhost:${PORT}`);
 });
