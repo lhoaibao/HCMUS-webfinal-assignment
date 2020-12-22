056bc95904d510f8b309bf4fdd72b6b0aad88088
Le Hoai Bao <lehoaibao081999@gmail.com>
2020-12-22 19:23:47 +0700

    add sql script: create_table_course create_table_category

---
+++ sql_script/create_table_category.sql
@@ -0,0 +1,5 @@
+create table if not exists category (
+    id int auto_increment not null,
+    category_name varchar(100),
+    primary key(id)
+)
---
+++ sql_script/create_table_course.sql
@@ -0,0 +1,15 @@
+create table if not exists course (
+    id int auto_increment not null,
+    category_id int not null,
+    course_name varchar(1000),
+    course_image LONGBLOB not null,
+    course_description text,
+    course_detail text,
+    rating_scoce decimal(2,2) not null,
+    rating_students int not null default 0,
+    enroll_number int not null default 0,
+    course_fee int not null,
+    last_modify datetime not null,
+    course_program LONGBLOB not null,
+    primary key(id)
+)
