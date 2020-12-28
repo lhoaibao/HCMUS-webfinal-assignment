8d11c0e48ddc8f6cff8488d36ee97158a039e505
Le Hoai Bao <lehoaibao081999@gmail.com>
2020-12-28 20:15:51 +0700

    change category.model add function single and add

--- utils/db.js
+++ utils/db.js
@@ -10,31 +10,3 @@ const pool = mysql.createPool({
 
 const poolQuery = util.promisify(pool.query.bind(pool));
 module.exports = { load: (sql) => poolQuery(sql) };
-
-/* Notes: we have 3 ways to handle asynchronous in javascript
-      Method 1: using callback function
-      Method 2: using promise
-      Method 3: using async/await
-*/
-
-/*Method 1: callback function
- load(sql, func) {
-   connection.connect();
-   connection.query(sql, function (error, result, field) {
-     if (error) throw error;
-     func(result);
-     connection.end();
-   });
- },
-*/
-
-/*Method 2: using Promise
- load(sql) {
-   return new Promise(function (done, fail) {
-     pool.query(sql, function (error, result, field) {
-       if (error) fail(error);
-       else done(result);
-     });
-   });
- }
- */
