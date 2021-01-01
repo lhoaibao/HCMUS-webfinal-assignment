b5ed7eb4b1988de0522b5b06d0ebd5cadbf71bdb
Le Hoai Bao <lehoaibao081999@gmail.com>
2021-01-01 18:29:27 +0700

    update utils/db.js

--- adminApp/AdminPage/utils/db.js
+++ adminApp/AdminPage/utils/db.js
@@ -1,12 +1,18 @@
-const mysql = require("mysql");
-const util = require("util");
+const mysql = require('mysql');
+const util = require('util');
+
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "elearning",
+  host: 'localhost',
+  user: 'root',
+  password: 'kaorikousei0208',
+  database: 'elearning',
   connectionLimit: 50,
 });
 
-const poolQuery = util.promisify(pool.query.bind(pool));
-module.exports = { load: (sql) => poolQuery(sql) };
+const pool_query = util.promisify(pool.query).bind(pool);
+
+module.exports = {
+  load: sql => pool_query(sql),
+  add: (entity, tableName) => pool_query(`insert into ${tableName} set ?`, entity),
+  del: (condition, tableName) => pool_query(`delete from ${tableName} where ?`, condition),
+  patch: (entity, condition, tableName) => pool_query(`update ${tableName} set ? where ?`, [entity, condition])
