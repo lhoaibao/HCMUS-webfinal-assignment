1ff5513cfa1381e85deed3192154b273d0474755
Le Hoai Bao <lehoaibao081999@gmail.com>
2021-01-01 19:55:56 +0700

    add user.model, update post register at account.route

--- adminApp/AdminPage/routes/front/account.route.js
+++ adminApp/AdminPage/routes/front/account.route.js
@@ -7,6 +7,22 @@ router.get('/register', async function (req, res) {
     res.render('vwAccount/register');
 })
 
+router.post('/register', async function (req, res) {
+    const hash = bcrypt.hashSync(req.body.password, 10);
+    const dob = moment(req.body.dob, 'DD/MM/YYYY').format('YYYY-MM-DD');
+    const user = {
+        username: req.body.username,
+        password: hash,
+        dob: dob,
+        name: req.body.name,
+        email: req.body.email,
+        permission: 0
+    }
+
+    await userModel.add(user);
+    res.render('vwAccount/register');
+})
+
 router.get('/login', async function (req, res) {
     res.render('vwAccount/login');
 })
