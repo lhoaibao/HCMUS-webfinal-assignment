d28a606d64e7cc07cf01ce2c1d26d39958f0c487
HungTrinh99 <minhhung.it99@gmail.com>
2021-01-04 15:30:45 +0700

    restructure main branch

--- sql_script/create_table_categories.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `category`;
-create table if not exists categories (
-    category_id int auto_increment not null,
-    category_name varchar(100),
-    primary key(category_id)
-);
