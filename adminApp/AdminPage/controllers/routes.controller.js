41500be0d1df82303ce9eba9bcbc8d1c7c2a0667
Le Hoai Bao <lehoaibao081999@gmail.com>
2021-01-17 08:37:00 +0700

    update home view

--- adminApp/AdminPage/controllers/routes.controller.js
+++ adminApp/AdminPage/controllers/routes.controller.js
@@ -1,13 +1,24 @@
 const auth = require('../controllers/auth.controller');
 
 const isAdmin = require('../controllers/admin.controller');
-
+const courseModel = require('../models/course.model');
+const subCategoryModel = require('../models/subCategory.model');
+const userModel = require('../models/user.model');
+const categoryModel = require('../models/category.model');
 
 module.exports = function (app) {
-    app.get('/', auth, function (req, res) {
+    app.get('/', auth, async function (req, res) {
         req.session.retUrl = req.originalUrl
+        courses = await courseModel.all('*')
+        users = await userModel.all()
+        categories = await categoryModel.all()
+        subcategories = await subCategoryModel.all()
         res.render('home', {
-            currentUser: req.session.authUser
+            currentUser: req.session.authUser,
+            numcourses: courses.length,
+            numusers: users.length,
+            numcategories: categories.length,
+            numsubcategories: subcategories.length
         });
     });
     app.use('/user', require('../routes/user.route'));
